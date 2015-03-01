a43cf440f4c1414eba7e95b8fd95a42d303a9704
Rohan <rohan@cs.utexas.edu>
2015-02-28 21:23:24 -0500

    fixed yelp stuff

--- foodme/client/templates/restaurants/restaurant_list.js
+++ foodme/client/templates/restaurants/restaurant_list.js
@@ -1,9 +1,17 @@
+Session.set("restaurants", null);
+
 Template.restaurantList.helpers({
-  currentLocation: function() {
-    navigator.geolocation.getCurrentPosition(function(loc) {
-      console.log(loc);
-      Meteor.call("getLocalRestaurants", loc.coords);
-    });
-    //Meteor.call("getLocalRestaurants");
+  localRestaurants: function() {
+    return Session.get("restaurants");
   }
-})
+});
+
+Template.restaurantList.created = function() {
+  console.log("Hello!");
+  navigator.geolocation.getCurrentPosition(function(loc) {
+    Meteor.call("getLocalRestaurants", loc.coords, function(err, res) {
+      console.log("result:", res);
+      Session.set("restaurants", res.businesses);
+    });
+  });
+}
--- foodme/lib/methods.js
+++ foodme/lib/methods.js
@@ -1,24 +1,19 @@
 Meteor.methods({
   getLocalRestaurants: function(loc) {
-    console.log("in a meteor method");
-    console.log("startPos:", loc);
-
-    var url = "http://api.yelp.com/v2/search";
     var auth = Accounts.loginServiceConfiguration.findOne({service: 'yelp'});
 
-    var Yelper = Meteor.npmRequire("yelp");
-    var yelper = new Yelper();
-
-    var yelp = yelper.createClient({
+    var yelp = Meteor.npmRequire("yelp").createClient({
       consumer_key: auth.consumerKey, 
       consumer_secret: auth.consumerSecret,
       token: auth.accessToken,
       token_secret: auth.accessTokenSecret,
     });
 
-    yelp.search({term: "food", location: "Montreal"}, function(error, data) {
-      console.log(error);
-      console.log(data);
-    });
-  }
+    var ll = loc.latitude + "," + loc.longitude;
+
+    var a_search = Async.wrap(yelp, "search");
+
+    var res = a_search({term: "food", ll: ll, limit: 5});
+    return res;
+  },
 })
