c7a489c75e45056693755c90ade743a79009b30c
arknave <arnavsastry@gmail.com>
2015-02-28 21:34:24 -0500

    this is so broken

--- foodme/lib/router.js
+++ foodme/lib/router.js
@@ -2,14 +2,9 @@ Router.configure({
   layoutTemplate: "layout"
 });
 
-<<<<<<< HEAD
-Router.route('/', "restaurantList");
-=======
 Router.route('/', 'landing');
 var pages = ['register', 'new-restaurant', 'find-group'];
 
 pages.forEach(function(page) {
   Router.route(page, page);
 });
-
->>>>>>> 67bb595f987f6b4243889784b778353a5538093c
